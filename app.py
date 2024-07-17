a4dcae7ae59e5d7d3a1f5268cd636f5723f914a2
Prakhar Ganeriwal <114392816+PRAKHARGAN@users.noreply.github.com>
2024-07-17 18:38:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import streamlit as st
 from drive import authenticate_gdrive, list_items
-from auth import show_login
 from ui import main_app
+from auth import show_login
 
 def main():
-    if show_login():
-        query_params = st.experimental_get_query_params()
-        auth_code = query_params.get('code', [None])[0]
+    query_params = st.experimental_get_query_params()
+    auth_code = query_params.get('code', [None])[0]
 
-        if 'service' not in st.session_state:
-            if auth_code:
-                service = authenticate_gdrive(auth_code)
-                st.session_state.service = service
-                st.experimental_rerun()
-            else:
-                auth_url = authenticate_gdrive()
-                st.write(f'Please go to the following URL to authenticate: [Authenticate here]({auth_url})')
+    if 'service' not in st.session_state:
+        if auth_code:
+            service = authenticate_gdrive(auth_code)
+            st.session_state.service = service
+            st.experimental_rerun()
+        else:
+            auth_url = authenticate_gdrive()
+            st.write(f'Please go to the following URL to authenticate: [Authenticate here]({auth_url})')
 
-        if 'service' in st.session_state:
+    if 'service' in st.session_state:
+        if show_login():
             main_app(st.session_state.service)
 
 if __name__ == "__main__":
